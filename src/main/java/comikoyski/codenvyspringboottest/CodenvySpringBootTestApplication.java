696c4e1362e08250d4af8dcfdf93b08547195575
ChristianSanPedro <ChristianSanPedro@intrapay-ph-w23>
2019-09-06 19:35:54 +0800

    initial commit

---
+++ src/main/java/comikoyski/codenvyspringboottest/CodenvySpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package comikoyski.codenvyspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenvySpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenvySpringBootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comikoyski/codenvyspringboottest/CodenvySpringBootTestApplicationTests.java
@@ -0,0 +1,16 @@
+package comikoyski.codenvyspringboottest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodenvySpringBootTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
