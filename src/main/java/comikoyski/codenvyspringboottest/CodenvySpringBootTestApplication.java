e9d7f1bb4e4991f4eef5b9775903c7d7ef2c4dd8
Christian San Pedro <christian_sanpedro@yahoo.com>
2019-09-06 13:29:55 +0000

    Updated

--- src/main/java/comikoyski/codenvyspringboottest/CodenvySpringBootTestApplication.java
+++ src/main/java/comikoyski/codenvyspringboottest/CodenvySpringBootTestApplication.java
@@ -2,6 +2,11 @@ package comikoyski.codenvyspringboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.util.StringUtils;
 
 @SpringBootApplication
 public class CodenvySpringBootTestApplication {
@@ -9,5 +14,16 @@ public class CodenvySpringBootTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodenvySpringBootTestApplication.class, args);
 	}
+}
 
+@RestController
+@RequestMapping("/hello")
+class HelloController{
+  @RequestMapping
+  public String hello(@RequestParam(required=false) String name){
+    if(StringUtils.isEmpty(name))
+      return "Hello World !\n";
+    else
+      return "Hello "+name+" !\n";
+  }
 }
